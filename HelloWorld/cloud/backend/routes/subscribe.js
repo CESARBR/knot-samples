a714e63ecf212bcb629b59364aee4a0e82dc6eca
Paulo Serra Filho <ptvsf@cesar.org.br>
2018-09-11 09:05:12 -0300

    HelloWorld: Receive value async
    Subscribe using socket.io to receive updates for the value.
    
    Change-Id: I12df03c47bd10a57cd36173295d9c7b8622124ca

---
+++ HelloWorld/cloud/backend/routes/subscribe.js
@@ -0,0 +1,30 @@
+// const express = require('express');
+const KNoTCloud = require('knot-cloud');
+
+// const router = express.Router();
+
+module.exports = client => async (req) => {
+  const { meshbluHost } = req;
+  const { meshbluPort } = req;
+  const { meshbluAuthUUID } = req;
+  const { meshbluAuthToken } = req;
+  const { deviceId } = req;
+
+  const cloud = new KNoTCloud(
+    meshbluHost,
+    meshbluPort,
+    meshbluAuthUUID,
+    meshbluAuthToken
+  );
+
+  try {
+    await cloud.connect();
+    await cloud.subscribe(deviceId);
+
+    cloud.on((data) => {
+      client.emit(deviceId, data);
+    });
+  } catch (err) {
+    console.log(err); // eslint-disable-line no-console
+  }
+};
