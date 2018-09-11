f9f2ed90eb0cacfe30d6f02e456b6bc7151ab9f9
Paulo Serra Filho <ptvsf@cesar.org.br>
2018-09-11 08:27:30 -0300

    HelloWorld: Get online devices
    Shows all the online devices and the current value.
    Yet unable to change the value.

---
+++ HelloWorld/cloud/backend/routes/getDevices.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('lodash');
+const KNoTCloud = require('knot-cloud');
+
+async function getOnlineDevices(cloud, devices) {
+
+  let onlineDevices = _.map(_.filter(devices, 'online'), async function(device) {
+    let data = await cloud.getData(device.id);
+    device['sensorid'] = data[0].data.sensor_id;
+    device['value'] = data[0].data.value;
+    return device;
+  })
+  return(Promise.all(onlineDevices));
+}
+
+router.get('/', async function(req, res) {
+  const cloud = new KNoTCloud(
+    req.headers.meshblu_host,
+    req.headers.meshblu_port,
+    req.headers.meshblu_auth_uuid,
+    req.headers.meshblu_auth_token
+  );
+
+  try {
+    await cloud.connect();
+    let devices = await cloud.getDevices();
+    console.log(devices);
+    let onlineDevices = await getOnlineDevices(cloud, devices);
+    await cloud.close();
+    res.status(200).send(onlineDevices);
+  } catch (err) {
+    let result = err;
+    error = true;
+    console.log(result);
+    res.status(400).send(result);
+  }
+});
+
+module.exports = router;
--- HelloWorld/cloud/frontend/src/App.js
+++ HelloWorld/cloud/frontend/src/App.js
@@ -1,18 +1,115 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import _ from 'lodash'
+import { Icon } from 'semantic-ui-react'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      result: []
+    };
+    this.getDevices = this.getDevices.bind(this);
+    this.showDevice = this.showDevice.bind(this);
+    this.createDeviceList = this.createDeviceList.bind(this);
+  }
+  showDevice(device) {
+    return(
+      <div className='online-device' id={device.id} key={device.id}>
+        <div className='device-info'>
+          <div className='device-name'>
+            {device.name}
+          </div>
+          <div className='device-id'>
+            {device.id}
+          </div>
+        </div>
+        <div className='device-value'>
+          {device.value ? <Icon name='lightbulb outline' color='yellow' size='massive' /> : <Icon name='lightbulb' color='black' size='massive' />}
+        </div>
+      </div>
+    );
+  }
+
+  createDeviceList() {
+    console.log(this.state.devices);
+    return (
+      <div id='online-devices'>
+        <h1 className='online-devices-header'>
+          ONLINE DEVICES
+        </h1>
+        {_.map(this.state.devices, this.showDevice)}
+      </div>
+    );
+  };
+
+  getDevices() {
+    if(!this.state.uuid){
+      window.alert('UUID is mandatory');
+      return;
+    }
+    if (!this.state.token) {
+      window.alert('TOKEN is mandatory')
+      return;
+    }
+
+    let myHeaders = new Headers({
+      "meshblu_auth_uuid": this.state.uuid,
+      "meshblu_auth_token": this.state.token,
+      "meshblu_host": this.state.host || 'knot-test.cesar.org.br',
+      "meshblu_port": this.state.port || '3000'
+    });
+
+    var myInit = { method: 'GET',
+               headers: myHeaders};
+
+    this.setState({
+    headers: myHeaders
+    });
+
+    fetch('getdevices', myInit)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        devices: json
+      })
+      _.map(json,this.updateDevice)
+    });
+  }
+
   render() {
+    const devices = this.state.devices;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='header-wrapper'>
+          <header className="App-header">
+            <h1 className="App-title">Welcome to KNoT</h1>
+          </header>
+        </div>
+        <div className="knot-info-wrapper">
+          <div className="knot-info">
+            <label htmlFor='host'> MESHBLU HOST </label>
+            <input type='text' id='host' className='knot-info-text' placeholder='knot-test.cesar.org.br'  onChange={(e)=> this.setState({host:e.target.value})}/>
+          </div>
+          <div className="knot-info">
+            <label htmlFor='port'> MESHBLU PORT </label>
+            <input type='text' id='port' className='knot-info-text' placeholder='3000' onChange={(e)=> this.setState({port:e.target.value})}/>
+          </div>
+          <div className="knot-info">
+            <label htmlFor='uuid'> UUID </label>
+            <input type='text' id='uuid'className='knot-info-text' onChange={(e)=> this.setState({uuid:e.target.value})}/>
+          </div>
+          <div className="knot-info">
+            <label htmlFor='token'> TOKEN </label>
+            <input type='text' id='token' className='knot-info-text' onChange={(e)=> this.setState({token:e.target.value})}/>
+          </div>
+          <button className='btn' onClick={this.getDevices}>
+          GET DEVICES
+          </button>
+          </div>
+        <div className='list-devices-wrapper'>
+          {_.isEmpty(devices) ? <div/> : <this.createDeviceList className='list-devices' />}
+        </div>
       </div>
     );
   }
